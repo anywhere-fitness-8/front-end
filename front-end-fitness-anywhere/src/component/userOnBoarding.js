593ba1dc87b422d047422dce1ed700ce88ff17e8
minasoha <minasoha@yahoo.com>
2021-09-20 15:36:15 -0400

    finished the onbording page need to update link once website is done

--- front-end-fitness-anywhere/src/component/userOnBoarding.js
+++ front-end-fitness-anywhere/src/component/userOnBoarding.js
@@ -1,13 +1,62 @@
 import React from "react";
 import styled from "styled-components";
+import {useHistory} from 'react-router-dom';
 
-const Container = styled.div``;
+const Container = styled.div`
+display: flex;
+  
+  justify-content: center;
+
+`;
+const SkipButton = styled.button`
+  
+  
+  color: tomato;
+  font-size: 1em;
+  margin: 1em;
+  padding: 0.25em 1em;
+  border: 2px solid tomato;
+  border-radius: 3px;
+  align-self: flex-end;
+ 
+`
+
 
 function CompUserOnboarding(props) {
+
+  const history = useHistory();
+
+  const onClick = () =>{
+    let path = `/profile`;
+    history.push(path)
+
+  }
+
+
+
   return (
     <Container>
-      <h3>CompUserOnboarding.js</h3>
+      <div>
+        <h3>Walkthrough tutorial</h3>
+
+        <iframe width="560" height="315" src="https://www.youtube.com/embed/dQw4w9WgXcQ" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"  allowfullscreen></iframe>
+
+        
+        <div className="skip-button">
+          <SkipButton onClick={onClick} >
+          
+            Skip!
+            
+          </SkipButton>
+        
+
+
+          
+        </div>
+        
+      </div>
     </Container>
+    
   );
 }
 
