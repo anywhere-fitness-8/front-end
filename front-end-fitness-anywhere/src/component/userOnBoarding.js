176cf793af95f768526757a7e5ff7ad229741ebd
beefybroccoli <tedkimemail@yahoo.com>
2021-09-20 02:31:35 -0700

    initial

---
+++ front-end-fitness-anywhere/src/component/userOnBoarding.js
@@ -0,0 +1,14 @@
+import React from "react";
+import styled from "styled-components";
+
+const Container = styled.div``;
+
+function CompUserOnboarding(props) {
+  return (
+    <Container>
+      <h3>CompUserOnboarding.js</h3>
+    </Container>
+  );
+}
+
+export default CompUserOnboarding;
