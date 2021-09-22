5066f132aee8dceca45ee6759996f3c98d89ec3e
beefybroccoli <tedkimemail@yahoo.com>
2021-09-22 06:17:50 -0700

    registion form validation added

--- src/component/registrationForm.js
+++ src/component/registrationForm.js
@@ -1,13 +1,14 @@
 import axios from "axios";
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { useHistory } from "react-router";
 import styled from "styled-components";
+import {
+  registrationFormSchema,
+  schema_validate_form,
+  schema_validate_input,
+} from "./schema_validation";
 
-
-
-
-const Container = styled.div`
-`;
+const Container = styled.div``;
 const Form = styled.div``;
 const Label = styled.div``;
 const Input = styled.input``;
@@ -19,110 +20,144 @@ const Select = styled.select``;
 const Option = styled.option``;
 
 const initialState = {
-    username:'',
-    password:'',
-    email:'',
-    remaining_classes: 0,
-    role_id: 1
-}
-const RegistrationForm = () =>{
+  username: "",
+  password: "",
+  email: "",
+  remaining_classes: 0,
+  role_id: 1,
+};
 
-  const {push} = useHistory()
+const initialStateValidationText = {
+  username: "",
+  password: "",
+  email: "",
+  role_id: "",
+};
+const RegistrationForm = () => {
+  const { push } = useHistory();
   const [stateFormData, set_stateFormData] = useState(initialState);
-  console.log(stateFormData)
+  const [stateValidationText, set_stateValidationText] = useState(
+    initialStateValidationText
+  );
+  const [stateValidationBoolean, set_stateValidationBoolean] = useState(false);
+  console.log(stateFormData);
 
   const cb_onChange = (event) => {
-      const { name, value } = event.target;
-      set_stateFormData({ ...stateFormData, [name]: value });
-
-    };
+    const { name, value } = event.target;
+    set_stateFormData({ ...stateFormData, [name]: value });
+    schema_validate_input(
+      registrationFormSchema,
+      name,
+      value,
+      stateValidationText,
+      set_stateValidationText
+    );
+  };
 
   const cb_onChangeInt = (event) => {
     const { name, value } = event.target;
+    schema_validate_input(
+      registrationFormSchema,
+      name,
+      value,
+      stateValidationText,
+      set_stateValidationText
+    );
     set_stateFormData({ ...stateFormData, [name]: parseInt(value) });
-
   };
 
+  useEffect(() => {
+    schema_validate_form(
+      registrationFormSchema,
+      stateFormData,
+      set_stateValidationBoolean
+    );
+  }, [stateFormData]);
 
-  const cb_onSubmit = (event) => {       
-      event.preventDefault()
+  const cb_onSubmit = (event) => {
+    event.preventDefault();
 
-      axios.post('https://anywhere-fitness-8.herokuapp.com/api/auth/register', stateFormData)
-      .then(res=>{
-          push('/login') 
+    axios
+      .post(
+        "https://anywhere-fitness-8.herokuapp.com/api/auth/register",
+        stateFormData
+      )
+      .then((res) => {
+        push("/login");
       })
-      .catch(err=>[
-          console.error(err)
-      ])
-  };   
+      .catch((err) => [console.error(err)]);
+  };
 
-
-  return(<Container>
+  return (
+    <Container>
       <h3>RegistrationForm.js</h3>
-    <Form>
-      {/* -----------------username input------------------------------- */}
-      <Label>
-        <b>Username : </b>
-        <Input
-          type="text"
-          name="username"
-          value={stateFormData.username}
-          onChange={cb_onChange}
-          placeholder="Must be 5 characters long"
-          autoFocus
-        />
-      </Label>
-      <ValidationText>placeholder: stateValidationText.username</ValidationText>
+      <Form>
+        {/* -----------------username input------------------------------- */}
+        <Label>
+          <b>Username : </b>
+          <Input
+            type="text"
+            name="username"
+            value={stateFormData.username}
+            onChange={cb_onChange}
+            placeholder="Must be 5 characters long"
+            autoFocus
+          />
+        </Label>
+        <ValidationText>{stateValidationText.username}</ValidationText>
 
-      {/* -----------------username input------------------------------- */}
-      <Label>
-        <b>E-Mail : </b>
-        <Input
-          type="email"
-          name="email"
-          value={stateFormData.email}
-          onChange={cb_onChange}
-          placeholder="Must be valid E-Mail address"
-          autoFocus
-        />
-      </Label>
-      <ValidationText>placeholder: stateValidationText.email</ValidationText>
-      
-      {/* -------------------password input---------------------------- */}
-      <Label>
-        <b>Password : </b>
-        <Input
-          type="password"
-          name="password"
-          value={stateFormData.password}
-          onChange={cb_onChange}
-          placeholder="Must be 5 characters long"
-        />
-      </Label>
-      <ValidationText>placeholder: stateValidationText.password</ValidationText>
-      {/* -------------------account type ---------------------------- */}
+        {/* -----------------username input------------------------------- */}
+        <Label>
+          <b>E-Mail : </b>
+          <Input
+            type="email"
+            name="email"
+            value={stateFormData.email}
+            onChange={cb_onChange}
+            placeholder="Must be valid E-Mail address"
+            autoFocus
+          />
+        </Label>
+        <ValidationText>{stateValidationText.email}</ValidationText>
 
-      <Label>
-        <b>Account Type : </b>
-        <Select
-          value={stateFormData.role_id}
-          name="role_id"
-          type='number'
-          onChange={cb_onChangeInt}
-        >
-          <Option value='' disabled selected >(Please select one)</Option>
-          <Option value={1}>Student</Option>
-          <Option value={2}>Instructor</Option>
-        </Select>
-      </Label>
-      <ValidationText>placeholder: stateValidationText.role_id</ValidationText>
+        {/* -------------------password input---------------------------- */}
+        <Label>
+          <b>Password : </b>
+          <Input
+            type="password"
+            name="password"
+            value={stateFormData.password}
+            onChange={cb_onChange}
+            placeholder="Must be 5 characters long"
+          />
+        </Label>
+        <ValidationText>{stateValidationText.password}</ValidationText>
+        {/* -------------------account type ---------------------------- */}
 
-      {/* -------------------submit button----------------------- */}
-      <Button onClick={cb_onSubmit}>
-        Register
-      </Button>
-    </Form>
-  </Container>)
-}
+        <Label>
+          <b>Account Type : </b>
+          <Select
+            value={stateFormData.role_id}
+            name="role_id"
+            type="number"
+            onChange={cb_onChangeInt}
+          >
+            <Option value="" disabled selected>
+              (Please select one)
+            </Option>
+            <Option value={1}>Student</Option>
+            <Option value={2}>Instructor</Option>
+          </Select>
+        </Label>
+        <ValidationText>{stateValidationText.role_id}</ValidationText>
 
-export default RegistrationForm
+        {/* -------------------submit button----------------------- */}
+        <Button disabled={!stateValidationBoolean} onClick={cb_onSubmit}>
+          Register
+        </Button>
+      </Form>
+    </Container>
+  );
+};
+
+export default RegistrationForm;
