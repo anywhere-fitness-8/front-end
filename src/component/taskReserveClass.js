aacd380f2401886a5773ba7b396bafc263f90efd
beefybroccoli <tedkimemail@yahoo.com>
2021-09-22 14:33:38 -0700

    fully functional serch

--- src/component/taskReserveClass.js
+++ src/component/taskReserveClass.js
@@ -25,26 +25,22 @@ const DIV_Right = styled.div`
 `;
 
 function CompTaskReserveClass(props) {
-  //--------------------------------------------------------
-  /*
-    stateArrayOfClasses store an array of classDetail object
-  */
+  //---------------------------------------------------------------------
+  //stateArrayOfClasses store an array of classDetail object
   const [stateArrayOfClasses, set_stateArrayOfClasses] = useState([]);
-  /*
-    stateArrayOfReservedClass store an array of class_id, which is key in classDetail object
-  */
+  //---------------------------------------------------------------------
+  //stateArrayOfReservedClass store an array of class_id, which is key in classDetail object
   const [stateArrayOfReservedClass, set_stateArrayOfReservedClass] = useState(
     []
   );
-
-  /*
-    stateArrayOfRenderedClasses store an array of classDetail object 
-  */
+  //---------------------------------------------------------------------
+  //stateArrayOfRenderedClasses store an array of classDetail object
   const [stateArrayOfRenderedClasses, set_stateArrayOfRenderedClasses] =
-    useState(list_of_classes);
+    useState([]);
   //---------------------------------------------------------------------
   //set_stateSearchCriteria allow user to search class
   const [stateSearchCriteria, set_stateSearchCriteria] = useState(null);
+  //---------------------------------------------------------------------
   //set_stateSelectedClass allow user to render a CompClassDetail in CompTaskManageClass or on "/manage" page
   const [stateSelectedClass, set_stateSelectedClass] = useState(null);
 
@@ -53,26 +49,11 @@ function CompTaskReserveClass(props) {
     //??????????????????????????????????????????????????????
     //Unit 3 - retrieve a list of classes from the server
     //??????????????????????????????????????????????????????
-    set_stateArrayOfClasses([...stateArrayOfClasses, ...list_of_classes]);
+    set_stateArrayOfClasses([...list_of_classes]);
 
-    //????????????????????????????????????????????????????????????????
-    // //render all class intially
-    // set_stateArrayOfRenderedClasses([
-    //   ...stateArrayOfRenderedClasses,
-    //   ...stateArrayOfClasses,
-    // ]);
+    //render all class intially
+    set_stateArrayOfRenderedClasses([...stateArrayOfClasses]);
   }, []);
-
-  //monitor change in stateSelectedClass
-  useEffect(() => {
-    // stateSelectedClass &&
-    // console.log("stateSelectedClass = ", stateSelectedClass);
-  }, [stateSelectedClass]);
-
-  //monitor change in stateArrayOfClasses
-  useEffect(() => {
-    // console.log("stateArrayOfClasses.length = ", stateArrayOfClasses.length);
-  }, [stateArrayOfClasses]);
 
   //monitor change in stateSearchCriteria
   useEffect(
@@ -85,24 +66,23 @@ function CompTaskReserveClass(props) {
         }
       };
 
-      //????????????????????????????????????????????????????????
-      //what happen after a new search criteria gets detected?
-      //????????????????????????????????????????????????????????
       if (stateSearchCriteria) {
+        //create a new array with matching criterias to SearchCriteria object
         const temp_array = stateArrayOfClasses.filter((eachClass) => {
           if (
-            // eachClass.name.includes(stateSearchCriteria.name) &&
-            // eachClass.startTime.includes(stateSearchCriteria.time) &&
-            // eachClass.date.includes(stateSearchCriteria.date) &&
-            // eachClass.duration.includes(stateSearchCriteria.duration) &&
-            // eachClass.type.includes(stateSearchCriteria.type) &&
-            // eachClass.intensityLevel.includes(stateSearchCriteria.intensityLevel) &&
+            helper_compare(eachClass.name, stateSearchCriteria.name) &&
+            helper_compare(eachClass.type, stateSearchCriteria.type) &&
+            helper_compare(eachClass.startTime, stateSearchCriteria.time) &&
+            helper_compare(eachClass.date, stateSearchCriteria.date) &&
+            helper_compare(eachClass.duration, stateSearchCriteria.duration) &&
+            helper_compare(
+              eachClass.intensityLevel,
+              stateSearchCriteria.intensityLevel
+            ) &&
             helper_compare(eachClass.location, stateSearchCriteria.location)
           ) {
             return eachClass;
-          } //end if
-
-          //"tom".include("") - return false
+          }
         });
 
         set_stateArrayOfRenderedClasses(temp_array);
@@ -157,53 +137,3 @@ function CompTaskReserveClass(props) {
 }
 
 export default CompTaskReserveClass;
-
-/*
-4. Authenticated `Instructor` can create update and delete a `class`. At a minimum, each `class` must have the following properties:
-
-- `Name`
-- `Type`
-- `Start time`
-- `Duration`
-- `Intensity level`
-- `Location`
-- `Current number of registered attendees`
-- `Max class size`
-
-5. Authenticated `client` can search for available classes. At a minimum, they must be able to search by the following criteria:
-
-- `class time`
-- `class date`
-- `class duration`
-- `class type`
-- `intensity level`
-- `class location`
-
-{name:"", time:"", date:"", duration:"", type:"",intensityLevel:"",location:""}
-
-6. Authenticated `instructor` can create virtual punch pass categories for each type of group fitness class they offer (yoga, insanity, RIPPED, pilates, etc.)
-
-
-base URL: https://anywhere-fitness-8.herokuapp.com/api
-
-[get] /classes
-  return [classObject, ...]
-  render a list of CompClassCard
-
-  [get] /classes/:class_id
-  return [classObject]
-  render a CompClassDetail
-  
-
-  [get] /classes/user/:user_id
-  return [classObject, ...]
-  render a CompListOfClasses
-
-name ===
-- `class time`===
-- `class date`===
-- `class duration`===
-- `class type`===
-- `intensity level`
-- `class location`===
-*/
